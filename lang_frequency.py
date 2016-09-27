c65d18c8618eebcae6c3f7a9b05712137b475b15
aleksei-g <lexa_prorock@mail.ru>
2016-09-27 13:01:51 +0300

    задача решена

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,28 @@
+import os.path
+import re
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'rt') as f:
+        data = f.read()
+    return data
 
 
 def get_most_frequent_words(text):
-    pass
+    words = text.lower().split()
+    words_count = [{'word': word, 'count': words.count(word)}
+                   for word in set(words) if not re.match('^\W$', word)]
+    words_count.sort(key=lambda d: d['count'], reverse=True)
+    for num, word in enumerate(words_count[0:10], start=1):
+        print('%s) \"%s\": %s' % (num, word['word'], word['count']))
 
 
 if __name__ == '__main__':
-    pass
+    while True:
+        filepath = input('Enter file path: ')
+        if os.path.exists(filepath):
+            break
+        else:
+            print('File not found!')
+    data = load_data(filepath)
+    get_most_frequent_words(data)
