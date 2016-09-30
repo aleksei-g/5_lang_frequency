22f4888f398549b80d12282bdd4d7e98d4bd5426
aleksei-g <lexa_prorock@mail.ru>
2016-09-30 15:18:32 +0300

    исправлены ошибки

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,28 +1,41 @@
 import os.path
-import re
+import sys
+import argparse
+import string
+from collections import Counter
+
+
+COUNT_WORD_TO_PRINT = 10
 
 
 def load_data(filepath):
+    if not os.path.exists(filepath):
+        return None
     with open(filepath, 'rt') as f:
         data = f.read()
     return data
 
 
 def get_most_frequent_words(text):
-    words = text.lower().split()
-    words_count = [{'word': word, 'count': words.count(word)}
-                   for word in set(words) if not re.match('^\W$', word)]
-    words_count.sort(key=lambda d: d['count'], reverse=True)
-    for num, word in enumerate(words_count[0:10], start=1):
-        print('%s) \"%s\": %s' % (num, word['word'], word['count']))
+    words = [word.strip(string.punctuation) for word in text.lower().split()]
+    return Counter(words).most_common(COUNT_WORD_TO_PRINT)
+
+
+def createParser():
+    parser = argparse.ArgumentParser(description='Поиск %s часто встречающихся\
+                                     слов в тексте.' % COUNT_WORD_TO_PRINT)
+    parser.add_argument('-f', '--file', required=True, metavar='ФАЙЛ',
+                        help='Путь до текстового файла.')
+    return parser
 
 
 if __name__ == '__main__':
-    while True:
-        filepath = input('Enter file path: ')
-        if os.path.exists(filepath):
-            break
-        else:
-            print('File not found!')
-    data = load_data(filepath)
-    get_most_frequent_words(data)
+    parser = createParser()
+    namespace = parser.parse_args()
+    data = load_data(namespace.file)
+    if not data:
+        print('Файл не найден!')
+        sys.exit()
+    words_count = get_most_frequent_words(data)
+    for num, word in enumerate(words_count[0:10], start=1):
+        print('%s) \"%s\": %s' % (num, word[0], word[1]))
